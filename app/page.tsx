40df2d40ed5195623d24fe5eb77285ff48f53539
Fillipo Madella <3812296+fillipo@users.noreply.github.com>
2024-10-26 15:20:05 -0400

    Add app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to our Farm-to-Table Marketplace!</h1>
+      <p>Connecting local farmers with conscious consumers.</p>
+      {/* Add more content here as the application grows */}
+    </div>
+  );
+};
+
+export default HomePage;
