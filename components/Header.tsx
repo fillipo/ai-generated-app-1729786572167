34dffbf8889260bd2990c4f8059fc7a3b2110b53
Fillipo Madella <3812296+fillipo@users.noreply.github.com>
2024-10-26 15:20:06 -0400

    Add components/Header.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-green-500 text-white p-4">
+      <h1 className="text-2xl font-bold">Farm-to-Table</h1>
+      {/* Add navigation links here */}
+    </header>
+  );
+};
+
+export default Header;
